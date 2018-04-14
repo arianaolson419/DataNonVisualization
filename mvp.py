f18ca174fe81d78577965fde782ca7454239182f
arianaolson419 <arianaolson419@gmail.com>
2018-04-13 23:33:36 -0400

    Add function definitions

---
+++ mvp.py
@@ -0,0 +1,31 @@
+"""This is an MVP
+"""
+import numpy as np
+import pandas as pd
+
+def get_csv_data():
+    """Extract x and y values from a csv file.
+    Parameters
+    ----------
+    filepath : the path to the file
+
+    Returns
+    -------
+    x : the x coordinates
+    y : the y coordinates
+    """
+    pass
+
+def find_slopes():
+    """finds the slopes between each point in the data
+    Parameters
+    ----------
+    x : the x coordinates of the data.
+    y : the y coordinates of the data
+
+    Returns
+    -------
+    slopes : a numpy array with each element being the slope between
+    consecutive points.
+    """
+    pass
